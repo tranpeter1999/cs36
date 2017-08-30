9d4e912324c6b3153c1e1af0c29d44ac80117bc9
Peter Tran <tranpeter1999@gmail.com>
2017-08-30 16:29:02 -0700

    Add files via upload

---
+++ programSet1.c
@@ -0,0 +1,20 @@
+/*
+This program finds the area and circumference of a circle, allowing the user to pick the radius of the circle.
+It then displays the radius, diameter, area, and circumference up to 5 decimal places.
+Written by Peter Tran
+8/30/17
+C. Loke
+CS36
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int r;
+
+    printf("Enter a radius!\n");
+    scanf("%d\n",&r);
+
+    printf("%d",r);
+}
