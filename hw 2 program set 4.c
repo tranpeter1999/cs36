e0275291b8a8f2dfd61b3190523180fd7bb86dd6
Peter Tran <tranpeter1999@gmail.com>
2017-10-02 15:29:15 -0700

    Update hw 2 program set 4.c

--- hw 2 program set 4.c
+++ hw 2 program set 4.c
@@ -14,22 +14,67 @@ void main()
 	double number1 = 0.0;	//First operand value a decimal number
 	double number2 = 0.0;	//Second operand value a decimal number
 	char operation = 0;		//Operation - must be +,-,*,/ or %
-	
+
 	printf("\nEnter the calculation\n");
 	scanf("%lf%c%lf",&number1,&operation,&number2);
-	
+
 	switch(operation)
 	{
-		case '+': 	printf("= %lf",number1 + number2);
-		case '-': 	printf("= %lf",number1 - number2);
-		case '*': 	printf("= %lf",number1 * number2);
+		case '+': 	printf("= %lf",number1 + number2); break;
+		case '-': 	printf("= %lf",number1 - number2); break;
+		case '*': 	printf("= %lf",number1 * number2); break;
 		case '/':	if(number2 != 0)
 						{
 							printf("= %lf",number1 / number2);
-							printf("\nThe remainder is %d",number1 % number2);
+							if((int) number1 == number1 && (int) number2 == number2)
+                                printf("\nWithout decimals = %d. The remainder would be %d",(int) number1 / (int) number2, (int) number1 % (int) number2);
+                            break;
 						}
 					else
-						printf("Division by zero error!");
-		default:	printf("Invalid");
+						printf("Division by zero error!"); break;
+		default:	printf("Invalid"); break;
 	}
 }
+
+/* OUTPUT
+Testing +
+
+Enter the calculation
+3+5.2
+= 8.200000
+Process returned 10 (0xA)   execution time : 3.342 s
+Press any key to continue.
+
+Testing -
+
+Enter the calculation
+9.23-2.0001
+= 7.229900
+Process returned 10 (0xA)   execution time : 7.287 s
+Press any key to continue.
+
+Testing *
+
+Enter the calculation
+612*0.1
+= 61.200000
+Process returned 11 (0xB)   execution time : 8.186 s
+Press any key to continue.
+
+Testing / without both int
+
+Enter the calculation
+5.3/2
+= 2.650000
+Process returned 3199 (0xC7F)   execution time : 2.245 s
+Press any key to continue.
+
+Testing / with both int
+
+Enter the calculation
+7/2
+= 3.500000
+Without decimals = 3. The remainder would be 1
+Process returned 47 (0x2F)   execution time : 2.826 s
+Press any key to continue.
+*/
