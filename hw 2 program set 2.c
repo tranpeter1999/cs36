0e110cda8017e19abfd83b3ba9375cbc570439cb
Peter Tran <tranpeter1999@gmail.com>
2017-10-02 13:36:29 -0700

    Create hw 2 program set 2.c

---
+++ hw 2 program set 2.c
@@ -0,0 +1,26 @@
+/*
+This program allows the user to enter a grade in one of alphabetic letters A, B, C, D, or F
+It then prints the corresponding score the numerical value.
+It accounts for both uppercase and lowercase
+*/
+
+int main()
+{
+	char grade;
+	
+	printf("Enter a grade {A,B,C,D,F]:   ");
+	scanf("%c",&grade);
+	
+	if(grade == 'A' || grade == 'a')
+		printf("\nThe score for the grade 'A' is 4.00");
+	else if(grade == 'B' || grade == 'b')
+		printf("\nThe score for the grade 'B' is 3.00");
+	else if(grade == 'C' || grade == 'c')
+		printf("\nThe score for the grade 'C' is 2.00");
+	else if(grade == 'D' || grade == 'd')
+		printf("\nThe score for the grade 'D' is 1.00");
+	else if(grade == 'F' || grade == 'f')
+		printf("\nThe score for the grade 'F' is 0.00");
+	
+	return 0;
+}
