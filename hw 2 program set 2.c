39c56b44718981b153b6e4674c728095b6847d76
Peter Tran <tranpeter1999@gmail.com>
2017-10-02 15:09:29 -0700

    Update hw 2 program set 2.c

--- hw 2 program set 2.c
+++ hw 2 program set 2.c
@@ -13,22 +13,22 @@ It accounts for both uppercase and lowercase
 int main()
 {
 	char grade;
-	
+
 	printf("Enter a grade {A,B,C,D,F]:   ");
 	scanf("%c",&grade);
-	
+
 	if(grade == 'A' || grade == 'a')
-		printf("\nThe score for the grade 'A' is 4.00");
+		printf("The score for the grade 'A' is 4.00");
 	else if(grade == 'B' || grade == 'b')
-		printf("\nThe score for the grade 'B' is 3.00");
+		printf("The score for the grade 'B' is 3.00");
 	else if(grade == 'C' || grade == 'c')
-		printf("\nThe score for the grade 'C' is 2.00");
+		printf("The score for the grade 'C' is 2.00");
 	else if(grade == 'D' || grade == 'd')
-		printf("\nThe score for the grade 'D' is 1.00");
+		printf("The score for the grade 'D' is 1.00");
 	else if(grade == 'F' || grade == 'f')
-		printf("\nThe score for the grade 'F' is 0.00");
+		printf("The score for the grade 'F' is 0.00");
 	else
-		printf("\nInvalid");
-	
+		printf("Invalid");
+
 	return 0;
 }
