8ad1c870246aef7c67755658f2930a4ac9c25746
Peter Tran <tranpeter1999@gmail.com>
2017-10-03 21:33:45 -0700

    Update hw 2 program set 1.c

--- hw 2 program set 1.c
+++ hw 2 program set 1.c
@@ -18,23 +18,50 @@ int main()
 {
 	int a,b,sum;
 	time_t t;
-	
+
 	srand((unsigned) time(&t));
-	
+
 	a = rand() %6 + 1;
 	b = rand() %6 + 1;
-	
+
 	sum = a + b;
-	
-	printf("Your sum is %d",&sum);
-	
-	switch(sum)
-	{
-		case 7: case 11: printf("You win!\n"); break;
-		case 2: case 12: printf("You lose!\n"); break;
-		case 1: case 3: case 4: case 5: case 6: case 8: case 9: case 10: printf("You get your money back!\n"); break;
-		default: printf("Invalid\n");
-	}
-	
+
+	printf("Your sum is %d\n",sum);
+
+	if(sum == 7 || sum == 11)
+        printf("You win!\n");
+    if(sum == 2 || sum == 12)
+        printf("You lose!\n");
+    if(sum > 0 && sum < 13 && sum != 2 && sum != 7 && sum != 11 && sum != 12)
+        printf("You get your money back!\n");
+    if(sum < 0 || sum > 12)
+		printf("Invalid\n");
+
 	return 0;
 }
+
+/* OUTPUT
+Test 1
+
+Your sum is 7
+You win!
+
+Process returned 0 (0x0)   execution time : 0.019 s
+Press any key to continue.
+
+Test 2
+
+Your sum is 3
+You get your money back!
+
+Process returned 0 (0x0)   execution time : 0.008 s
+Press any key to continue.
+
+Test 3
+
+Your sum is 12
+You lose!
+
+Process returned 0 (0x0)   execution time : 0.018 s
+Press any key to continue.
+*/
