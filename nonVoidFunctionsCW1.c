3ea7022ddb1dd3caa529979947909679199fe735
Peter Tran <tranpeter1999@gmail.com>
2017-10-18 15:03:16 -0700

    Create nonVoidFunctionsCW1.c

---
+++ nonVoidFunctionsCW1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float findbig(float n,float b, float c) //float is non-void
+{
+    float big;
+        if (a>b && a>=c)
+            big=a
+        if (b>=a && b>=c)
+            big=b;
+        if (c>=a && c>=b)
+            big=c;
+        return big;//returns through non-void statement 
+        
+}
+
