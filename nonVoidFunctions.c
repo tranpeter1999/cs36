c89abbb686bd13bf2b699c53d8b3dbb745289961
Peter Tran <tranpeter1999@gmail.com>
2017-10-18 15:02:03 -0700

    Create nonVoidFunctions.c

---
+++ nonVoidFunctions.c
@@ -0,0 +1,51 @@
+Notes:
+Only functions on the exam. 10/25
+
+Review of function:
+Use function when you want to reuse your code.
+Reduce code to smaller pieces.
+one specific job. 
+
+return 2 values or more functions, must use void and pass by reference. 
+
+return 1 value you must use a non-void function. 
+
+non-void functions:
+return exactly one value
+use the return statement
+
+CW1
+
+CW2
+
+2 ways to call a non-void function
+1) using assign statement
+2) use the printf();
+
+
+LAB 1
+Calc Letter Grade
+
+90-100 A
+80-89 B
+60-79 C
+1-59 F
+else I
+
+Go back and recreate TRIANGLE/CIRCLE/RECTANGLE with single non-void functions. LAB2
+
+Function scope:
+Variable is active inside the function when the function is running(local variable).
+
+Program scope: 
+Variable is available throughout the whole program that is declared (global variable).
+
+File Scope:
+when a global variable is accessible until the end of the file.
+		static in x=10;
+		
+a global static variable can be used anywhere in the file in which it is declared but it is not accessbile by any other file.
+useful when the programmer writes own header file. 
+
+
+
